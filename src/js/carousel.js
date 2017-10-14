7a181e9cd15d8e6ff5b9766d6535ed270352d25b
stormraider2495 <am.stormraider2495@gmail.com>
2017-10-14 13:38:07 +0530

    changed ui

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,142 +1,154 @@
 import $ from 'jquery'
 export default class Carousel {
 
-    constructor(carouselElem, instructionElem) {
-        this.currentSlide = 0;
-        this.data = {};
-        this.repeat;
-        this.carouselElem = carouselElem;
-        this.instructionElem = instructionElem;
-    }
+	constructor( carouselElem, instructionElem ) {
+		this.currentSlide = 0;
+		this.data = {};
+		this.repeat;
+		this.carouselElem = carouselElem;
+		this.instructionElem = instructionElem;
+	}
 
-    getData(dataUrl) {
-        return $.ajax({
-            url: dataUrl,
-            type: "GET",
-            success: (data, status, xhr) => {
-                return data;
-            }
-        });
-    };
+	getData( dataUrl ) {
+		return $.ajax( {
+			url: dataUrl,
+			type: "GET",
+			success: ( data, status, xhr ) => {
+				return data;
+			}
+		} );
+	};
 
-    startCarousel(url) {
+	startCarousel( url ) {
 
-        let data = this.getData(url),
-            self = this;
-        data.then((resp) => {
-                self.data = resp;
-                self.repeat = resp.repeat;
-                self.showInstructionTxt(resp.instruction);
-                self.createCarouselSlides(resp.slides);
-                self.initializeCarouselLibrary();
-                self.bindCarouselEvents();
-                // self.normalizeCarouselSlideHeights(self.carouselElem.find(".item"));
-            })
-            .catch((err) => {});
-    }
+		let data = this.getData( url ),
+			self = this;
+		data.then( ( resp ) => {
+				self.data = resp;
+				self.repeat = resp.repeat;
+				self.showInstructionTxt( resp.instruction );
+				self.createCarouselSlides( resp.slides );
+				self.createCarouselIndicators();
+				self.initializeCarouselLibrary();
+				self.bindCarouselEvents();
+				// self.normalizeCarouselSlideHeights(self.carouselElem.find(".item"));
+			} )
+			.catch( ( err ) => {} );
+	}
 
-    initializeCarouselLibrary() {
-        let config = {
-            interval: false,
-            pause: false,
-            wrap: this.repeat
-        }
-        this.manageCarousel(config);
-    }
+	initializeCarouselLibrary() {
+		let config = {
+			interval: false,
+			pause: false,
+			wrap: this.repeat
+		}
+		this.manageCarousel( config );
+	}
 
-    manageCarousel(options) {
-        this.carouselElem.carousel(options);
-    }
+	manageCarousel( options ) {
+		this.carouselElem.carousel( options );
+	}
 
-    createCarouselSlides(multimediaContent) {
-        for (let i in multimediaContent) {
-            let className = (i > 0) ? "item" : "item active",
-                slide = '<div class="' + className + '">';
-            switch (multimediaContent[i].type) {
-                case "image":
-                    slide += '<img src="' + multimediaContent[i].link + '" alt="">';
-                    break;
-                case "audio":
-                    slide += '<audio controls> <source src="' + multimediaContent[i].link + '" type="audio/mpeg"> </audio>';
-                    break;
-                case "video":
-                    slide += '<video controls> <source src="' + multimediaContent[i].link + '" type="video/mp4"> </video>';
-                    break;
-            }
-            // slide += '<div class="carousel-caption"><p class="caption-text" aria-live="polite">' + multimediaContent[i].caption + '</p></div>';
-            slide += '</div>';
-            this.carouselElem.find(".carousel-inner").append(slide);
-        }
-        this.showSlideCaption(0);
-    }
+	createCarouselSlides( multimediaContent ) {
+		for ( let i in multimediaContent ) {
+			let className = ( i > 0 ) ? "item" : "item active",
+				slide = '<div class="' + className + '">';
+			switch ( multimediaContent[ i ].type ) {
+			case "image":
+				slide += '<img src="' + multimediaContent[ i ].link + '" alt="">';
+				break;
+			case "audio":
+				slide += '<audio controls> <source src="' + multimediaContent[ i ].link + '" type="audio/mpeg"> </audio>';
+				break;
+			case "video":
+				slide += '<video controls> <source src="' + multimediaContent[ i ].link + '" type="video/mp4"> </video>';
+				break;
+			}
+			// slide += '<div class="carousel-caption"><p class="caption-text" aria-live="polite">' + multimediaContent[i].caption + '</p></div>';
+			slide += '</div>';
+			this.carouselElem.find( ".carousel-inner" ).append( slide );
+		}
+		this.showSlideCaption( 0 );
+	}
 
-    bindCarouselEvents() {
-        let self = this;
-        this.carouselElem.find(".left").off('click').on("click", () => {
-            self.manageCarousel("prev");
-        });
+	createCarouselIndicators() {
+		let indicators = "",
+			target = '#' + this.carouselElem.attr( "id" );
+		for ( let i in this.data.slides ) {
+			let className = ( i > 0 ) ? "" : "active";
+			indicators += '<li data-target="' + target + '" data-slide-to="' + i + '" class="' + className + '"></li>';
+		}
+		this.carouselElem.find( ".carousel-indicators" ).append( indicators );
+	}
 
-        this.carouselElem.find(".right").off('click').on("click", () => {
-            self.manageCarousel("next");
-        });
+	bindCarouselEvents() {
+		let self = this;
+		this.carouselElem.find( ".left" ).off( 'click' ).on( "click", () => {
+			self.manageCarousel( "prev" );
+		} );
 
-        this.carouselElem.on('slide.bs.carousel', () => {
-            self.stopMediaPlaying();
-        });
+		this.carouselElem.find( ".right" ).off( 'click' ).on( "click", () => {
+			self.manageCarousel( "next" );
+		} );
 
-        this.carouselElem.on('slid.bs.carousel', () => {
-            self.showSlideCaption(self.getCurrentSlideNumber());
-        });
+		this.carouselElem.on( 'slide.bs.carousel', () => {
+			self.stopMediaPlaying();
+		} );
 
-        $(window).on('resize orientationchange', () => {
-            self.normalizeCarouselSlideHeights(self.carouselElem.find(".item"));
-        });
+		this.carouselElem.on( 'slid.bs.carousel', () => {
+			self.showSlideCaption( self.getCurrentSlideNumber() );
+		} );
 
-        $(".item").children().on('load', () => {
-            self.normalizeCarouselSlideHeights(self.carouselElem.find(".item"));
-        });
-    }
+		$( window ).on( 'resize orientationchange', () => {
+			self.normalizeCarouselSlideHeights( self.carouselElem.find( ".item" ) );
+		} );
 
-    showInstructionTxt(val) {
-        this.instructionElem.text(val);
-    }
+		$( ".item" ).children().on( 'load', () => {
+			self.normalizeCarouselSlideHeights( self.carouselElem.find( ".item" ) );
+		} );
+	}
 
-    showSlideCaption(slideNumber) {
-        this.carouselElem.find(".caption-text").text(this.data.slides[slideNumber].caption);
-    }
+	showInstructionTxt( val ) {
+		this.instructionElem.text( val );
+	}
 
-    getCurrentSlideNumber() {
-        return this.carouselElem.find('.item.active').index() - 1;
-    }
-    stopMediaPlaying() {
-        if ($(".item.active audio").length > 0) {
-            $(".item.active audio")[0].currentTime = 0;
-            $(".item.active audio")[0].pause();
-        }
-        if ($(".item.active video").length > 0) {
-            $(".item.active video")[0].currentTime = 0;
-            $(".item.active video")[0].pause();
-        }
-    }
+	showSlideCaption( slideNumber ) {
+		this.carouselElem.find( ".caption-text" ).text( this.data.slides[ slideNumber ].caption );
+	}
 
-    // Normalize Carousel Heights - pass in Bootstrap Carousel slides.
-    normalizeCarouselSlideHeights(slides) {
-        let items = slides, //grab all slides
-            heights = [], //create empty array to store height values
-            tallest = 0; //create variable to make note of the tallest slide
+	getCurrentSlideNumber() {
+		return this.carouselElem.find( '.item.active' ).index() - 1;
+	}
 
-        items.each(() => {
-            items.css('min-height', '0'); //reset min-height
-        });
+	stopMediaPlaying() {
+		if ( $( ".item.active audio" ).length > 0 ) {
+			$( ".item.active audio" )[ 0 ].currentTime = 0;
+			$( ".item.active audio" )[ 0 ].pause();
+		}
+		if ( $( ".item.active video" ).length > 0 ) {
+			$( ".item.active video" )[ 0 ].currentTime = 0;
+			$( ".item.active video" )[ 0 ].pause();
+		}
+	}
 
-        items.each((index, value) => { //add heights to array
-            heights.push(items.height());
-        });
+	// Normalize Carousel Heights - pass in Bootstrap Carousel slides.
+	normalizeCarouselSlideHeights( slides ) {
+		let items = slides, //grab all slides
+			heights = [], //create empty array to store height values
+			tallest = 0; //create variable to make note of the tallest slide
 
-        tallest = Math.max.apply(null, heights); //store largest value
+		items.each( () => {
+			items.css( 'min-height', '0' ); //reset min-height
+		} );
 
-        items.each(() => {
-            items.css('min-height', tallest + 'px');
-        });
-    }
+		items.each( ( index, value ) => { //add heights to array
+			heights.push( items.height() );
+		} );
+
+		tallest = Math.max.apply( null, heights ); //store largest value
+
+		items.each( () => {
+			items.css( 'min-height', tallest + 'px' );
+		} );
+	}
 }
